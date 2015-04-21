6c8df4c53a1f9e6a3e52a61046eba140241366a7
VISHAL GUPTA <vishal.gupta@intelligrape.com>
2015-04-21 22:12:41 +0530

    Initial Commit

---
+++ sampleSolver/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sampleSolver
+//
+//  Created by abhayam rastogi on 4/21/15.
+//  Copyright (c) 2015 Itelligrape. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
